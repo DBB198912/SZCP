aca69a5f8950e7911147abe1c5ab26d14bd0f442
DBB198912 <519223235@qq.com>
2022-04-14 10:06:17 +0800

    文档提交

---
+++ doc/1-需求设计/原型设计/数字藏品商城原型图20220409/files/商户用户业务流程图/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="商户用户业务流程图.html",d="generationDate",e=new Date(1649496074036.83),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="98356d49937e4d6ca8f8f92da0a724a8",q="type",r="Axure:Page",s="name",t="商户用户业务流程图",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="8efd81fd7016492b9c9dbd6cd49bbacb",Y="label",Z="friendlyType",ba="图片",bb="imageBox",bc="styleType",bd="visible",be=true,bf="75a91ee5b9d042cfa01b8d565fe289c0",bg="size",bh="width",bi=722,bj="height",bk=1048,bl="imageOverrides",bm="images",bn="normal~",bo="images/商户用户业务流程图/u2624.png",bp="masters",bq="objectPaths",br="8efd81fd7016492b9c9dbd6cd49bbacb",bs="scriptId",bt="u2624";
+return _creator();
+})());
